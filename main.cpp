cd1e5cd72fa9fa8e50019be60ffd1ecd5f5d75e1
Krystian Rodzaj <krystian.rodzaj@partner.bmw.de>
2024-10-09 00:18:39 +0200

    Add main test code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include "Server/inc/tcp_ip_server.h"
+#include <arpa/inet.h>
+#include <memory>
 
 int main()
 {
+    Server::Config::TcpIpServerConfig config;
+    config.socket_domain = AF_INET;
+    config.socket_type = SOCK_STREAM;
+    config.socket_protocol = 0;
+    config.ip_adress = "127.0.0.1";
+    config.socket_port = 8080;
+    config.max_connections = 10;
+
+    std::unique_ptr<Server::Interface::ITcpIpServer> server = std::make_unique<Server::TcpIpServer>(config);
+
+    server->LaunchServer();
 
 
     return 0;
