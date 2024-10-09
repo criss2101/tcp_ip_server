f682ff6893d3ce0e0d76563bc99b557911077d95
Krystian Rodzaj <krystian.rodzaj@partner.bmw.de>
2024-10-09 20:43:32 +0200

    Set required ports, and set listening socket to be unblocking

--- main.cpp
+++ main.cpp
@@ -6,16 +6,15 @@ int main()
 {
     Server::Config::TcpIpServerConfig config;
     config.socket_domain = AF_INET;
-    config.socket_type = SOCK_STREAM;
+    config.socket_type = SOCK_STREAM | SOCK_NONBLOCK;
     config.socket_protocol = 0;
     config.ip_adress = "127.0.0.1";
-    config.socket_port = 8080;
+    config.socket_port = 2345;
     config.max_connections = 10;
 
     std::unique_ptr<Server::Interface::ITcpIpServer> server = std::make_unique<Server::TcpIpServer>(config);
-
     server->LaunchServer();
 
-
+    server->ShutdownServer();
     return 0;
 }
