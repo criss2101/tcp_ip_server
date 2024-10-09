26ff850b27952463b1d064c789ed9e9fa6ca6f6a
Krystian Rodzaj <krystian.rodzaj@partner.bmw.de>
2024-10-09 23:52:43 +0200

    Add command processor, responsible for payload processing base on CommandID

--- Server/inc/command_processor.h
+++ Server/inc/command_processor.h
@@ -1,12 +1,48 @@
 #ifndef SERVER_INC_COMMAND_PROCESSOR_H
 #define SERVER_INC_COMMAND_PROCESSOR_H
 
+#include <functional>
+#include <variant>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <cstddef>
 
 namespace Server
 {
     namespace Processing
     {
+        namespace Interface
+        {
+            using CommandResult = std::variant<std::string, double>;
 
+            enum class CommandID
+            {
+                STRING_OUT = 1,
+                DOUBLE_OUT = 2,
+            };
+
+            class ICommandProcessor
+            {
+                public:
+                virtual CommandResult ProcessCommand(const CommandID command_id, const std::vector<std::byte>& payload) = 0;
+            };
+        }
+
+        class CommandProcessor : public Interface::ICommandProcessor
+        {
+            public:
+            using CommandHandler = std::function<Interface::CommandResult(const std::vector<std::byte>&)>;
+
+            CommandProcessor();
+
+            Interface::CommandResult ProcessCommand(Interface::CommandID commandID, const std::vector<std::byte>& payload) override;
+
+            private:
+            void RegisterCommandHandlers();
+
+            std::unordered_map<Interface::CommandID, CommandHandler> command_handlers;
+        };
     }  // namespace Processing
 } // namespace Server
 
--- Server/src/command_processor.cpp
+++ Server/src/command_processor.cpp
@@ -1,10 +1,49 @@
 #include "../inc/command_processor.h"
 
-
 namespace Server
 {
     namespace Processing
     {
+        CommandProcessor::CommandProcessor()
+        {
+            RegisterCommandHandlers();
+        }
 
+        Interface::CommandResult CommandProcessor::ProcessCommand(const Interface::CommandID command_id, const std::vector<std::byte>& payload)
+        {
+            auto it = command_handlers.find(command_id);
+            if (it != command_handlers.end())
+            {
+                return it->second(payload);
+            }
+            else
+            {
+                throw std::out_of_range("Received command_id is out of range");
+            }
+        }
+
+        void CommandProcessor::RegisterCommandHandlers()
+        {
+            command_handlers[Interface::CommandID::STRING_OUT] = [](const std::vector<std::byte>& payload)
+            {
+                std::string str(reinterpret_cast<const char*>(payload.data()), payload.size());
+
+                return str;
+            };
+
+            command_handlers[Interface::CommandID::DOUBLE_OUT] = [](const std::vector<std::byte>& payload)
+            {
+                if(payload.size() != sizeof(uint64_t))
+                {
+                    throw std::out_of_range("Payload is insufficient to be read as sizeof(uint64_t)");
+                }
+
+                const uint64_t value = *reinterpret_cast<const uint64_t*>(payload.data());
+
+                const double result = value / 1000;
+
+                return result;
+            };
+        }
     }  // namespace Processing
 } // namespace Server
