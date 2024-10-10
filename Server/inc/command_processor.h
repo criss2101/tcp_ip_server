17ac31d3caa5e8bb6e72f87d1dd19f56d06f34d3
Krystian Rodzaj <krystian.rodzaj@partner.bmw.de>
2024-10-10 21:56:45 +0200

    Add documentation for command_processor

--- Server/inc/command_processor.h
+++ Server/inc/command_processor.h
@@ -16,6 +16,11 @@ namespace Server
         {
             using CommandResult = std::variant<std::string, double>;
 
+            /**
+             * @brief Enum representing the command identifiers that can be processed.
+             * STRING_OUT - command that outputs a string result.
+             * DOUBLE_OUT - command that outputs a floating-point number.
+             */
             enum class CommandID
             {
                 STRING_OUT = 1,
@@ -27,6 +32,13 @@ namespace Server
                 public:
                 virtual ~ICommandProcessor() = default;
 
+                /**
+                 * @brief Processes a command based on the command ID and payload data.
+                 *
+                 * @param command_id The identifier of the command to be processed.
+                 * @param payload The data to be processed, encoded as a vector of int8_t.
+                 * @return CommandResult The result of processing the command, which is variant, check CommandResult.
+                 */
                 virtual CommandResult ProcessCommand(const CommandID command_id, const std::vector<int8_t>& payload) = 0;
             };
         }
@@ -38,9 +50,18 @@ namespace Server
 
             CommandProcessor();
 
+            CommandProcessor(const CommandProcessor&) = delete;
+            CommandProcessor& operator=(const CommandProcessor&) = delete;
+            CommandProcessor(CommandProcessor&&) = delete;
+            CommandProcessor& operator=(CommandProcessor&&) = delete;
+
             Interface::CommandResult ProcessCommand(Interface::CommandID commandID, const std::vector<int8_t>& payload) override;
 
             private:
+            /**
+             * @brief Registers the command handlers for each command ID.
+             * This allows the processor to associate command IDs with specific handlers.
+             */
             void RegisterCommandHandlers();
 
             std::unordered_map<Interface::CommandID, CommandHandler> command_handlers;
